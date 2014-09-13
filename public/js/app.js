61c86d547475f188ee88734fdf3ed0de755ddab3
JT Cho <jonathan.t.cho@gmail.com>
2014-09-13 16:02:40 -0400

    Adding written specification.

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+; //exit any previously loaded js
+
+//Main funct.
+jQuery(function($) {
+    'use strict';
+
+    /*
+     * IO Handling
+     * -----------
+     * Handles IO 
+     */
+    var IO = {
+
+    };
+
+    /*
+     *
+     */
+    var App = {
+
+    };
+
+    IO.init();
+    App.init();
+
+}($));
